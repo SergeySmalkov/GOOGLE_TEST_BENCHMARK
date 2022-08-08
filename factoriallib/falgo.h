cec97bafb1a997499c17a6ae728109e6e7803364
Sergey Smalkov <smalkov2012@yandex.ru>
2022-08-08 19:30:23 +0300

    Initial commit

---
+++ factoriallib/falgo.h
@@ -0,0 +1,10 @@
+#pragma once
+#define FACTORIALLIB_FALGO_H
+
+#define ll long long
+
+namespace falgo {
+    ll simple_recursion(ll n);
+    ll fact_naive(ll n);
+}
+
---
+++ main.cpp
@@ -0,0 +1,8 @@
+#include <iostream>
+#include "falgo.h"
+
+int main() {
+    std::cout << "Hello";
+    std::cout << "factorial:" << falgo::fact_naive(10);
+    return 0;
+}
